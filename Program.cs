e56b4537148789243e440c5eb6834e788cca001a
David Pasichny <d.pasichniy@chillicode.ru>
2021-07-07 18:38:32 +0300

    Реализована функция ввода данных и базовая логика

--- Program.cs
+++ Program.cs
@@ -10,6 +10,27 @@ namespace SimpleCalculatorHW
     {
         static void Main(string[] args)
         {
+            double firstInputValue,secondInputValue,result;
+
+            Console.WriteLine("Введите число");
+            firstInputValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите необходимы оператор: +,-,/,*");
+            ConsoleKey inputkey = Console.ReadKey().Key;
+
+            switch (inputkey)
+            {
+                case ConsoleKey.Add:
+                case ConsoleKey.OemPlus:
+                    Console.WriteLine("Введите число");
+                    secondInputValue = double.Parse(Console.ReadLine());
+                    result = firstInputValue + secondInputValue;
+                    Console.WriteLine("Результат равен " + result);
+                    break;
+                default:
+                    Console.WriteLine("Введён некоректный оператор " + inputkey);
+                    break;
+            }
         }
     }
 }
